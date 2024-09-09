0177d74e181cdebbf1e29af2d2ec0fdce8f36406
Nemo-0o0 <phuocvu.hcmc@gmail.com>
2024-09-09 11:04:41 +0700

    dang ki dang nhap , Postman  + database

--- apiRouter.js
+++ apiRouter.js
@@ -1,21 +1,21 @@
-const express = require('express')
-var router = express.Router()
+// const express = require('express')
+// var router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('GET!')
-})
+// router.get('/', (req, res) => {
+//     res.send('GET!')
+// })
 
-router.post('/', (req, res) => {
-    console.log(req.headers)
-    res.json('POST!' + req.body.username)
-})
+// router.post('/', (req, res) => {
+//     console.log(req.headers)
+//     res.json('POST!' + req.body.username)
+// })
 
-router.put('/', (req, res) => {
-    res.send('PUT!')
-})
+// router.put('/', (req, res) => {
+//     res.send('PUT!')
+// })
 
-router.delete('/', (req, res) => {
-    res.send('DELETE!')
-})
+// router.delete('/', (req, res) => {
+//     res.send('DELETE!')
+// })
 
-module.exports = router
+// module.exports = router
--- server.js
+++ server.js
@@ -1,17 +1,63 @@
 const express = require('express');
 const app = express()
-const port = 3000
 var bodyParser = require('body-parser')
+var port = 3000;
+const AccountModel = require('./models/account')
 
-var router = require('./apiRouter')
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
 app.use(bodyParser.json())
 
-app.use('/admin/api/', router)
+// Dang ki 
+app.post('/register', (req, res, next) => {
+    var username = req.body.username
+    var password = req.body.password
+
+    AccountModel.findOne({
+        username: username
+    })
+        .then(data => {
+            if (data) {
+                res.status(400).json('Tai khoan da ton tai')
+            } else {
+                return AccountModel.create({
+                    username: username,
+                    password: password
+                })
+            }
+        })
+        .then(data => {
+            res.json('Tao Thanh cong')
+        })
+        .catch(err => {
+            res.status(500).json('Tao tai khoan that bai')
+        })
+    console.log(username, password)
+})
+
+// Dang nhap
+app.post('/login', (req, res, next) => {
+    var username = req.body.username
+    var password = req.body.password
+
+    AccountModel.findOne({
+        username: username,
+        password: password
+    })
+        .then(data => {
+            if (data) {
+                res.json('Dan nhap thanh cong')
+            } else {
+                res.status(500).json('Dang nhap khong thanh cong')
+            }
+        })
+        .catch(err => {
+            res.status(401).json('Sai ten dang nhap hoac mat khau')
+        })
+})
+app.get('/', (req, res, next) => {
+    res.send('Home')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
