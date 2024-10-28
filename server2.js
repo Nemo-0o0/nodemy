f1d98d2253e10ffd0cad6ab4864fa19b9e527927
Nemo-0o0 <phuocvu.hcmc@gmail.com>
2024-10-28 16:08:56 +0700

    Giới thiệu về CORS, và cách mở CORS trong express server

--- server2.js
+++ server2.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express()
 const path = require('path')
 var bodyParser = require('body-parser')
-var port = 3000;
-// const AccountModel = require('./models/account')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -16,59 +14,6 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     next();
-})
-
-// Dang ki 
-app.post('/register', (req, res, next) => {
-    var username = req.body.username
-    var password = req.body.password
-
-    AccountModel.findOne({
-        username: username
-    })
-        .then(data => {
-            if (data) {
-                res.status(400).json('Tai khoan da ton tai')
-            } else {
-                return AccountModel.create({
-                    username: username,
-                    password: password
-                })
-            }
-        })
-        .then(data => {
-            res.json('Tao Thanh cong')
-        })
-        .catch(err => {
-            res.status(500).json('Tao tai khoan that bai')
-        })
-    console.log(username, password)
-})
-// Dang nhap
-app.post('/login', (req, res, next) => {
-    var username = req.body.username
-    var password = req.body.password
-
-    AccountModel.findOne({
-        username: username,
-        password: password
-    })
-        .then(data => {
-            if (data) {
-                res.json('Dan nhap thanh cong')
-            } else {
-                res.status(500).json('Dang nhap khong thanh cong')
-            }
-        })
-        .catch(err => {
-            res.status(401).json('Sai ten dang nhap hoac mat khau')
-        })
-})
-
-var accountRouter = require('./Router/router')
-app.use('/api/account/', accountRouter)
-app.get('/', (req, res, next) => {
-    res.send('Home')
 })
 
 // Pagination
@@ -126,6 +71,4 @@ app.get('/user', (req, res, next) => {
     }
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.listen(4000)
